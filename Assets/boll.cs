e4bcae06fc5db671c5389ab305fd26b1774d0fad
linnea.gyllensvaan <linnea.gyllensvaan@elev.ga.lbs.se>
2018-10-15 20:22:22 +0200

    älska

--- Assets/boll.cs
+++ Assets/boll.cs
@@ -8,8 +8,11 @@ public class boll : MonoBehaviour
     public Transform other;
     public Color left;
     public Color right;
-
+    public float Timer;
+    private float variabel = 1;
+    public float randomspeed;
     public float newposition;
+    public bool halafart;
 
 
     [Range(-720, 720)]
@@ -20,29 +23,31 @@ public class boll : MonoBehaviour
     {
         rend.color = left;
         rend.color = new Color(1f, 0.5f, 0.3f);
+        transform.position = new Vector3(Random.Range(-9, 4), Random.Range(-5, 1)); // startar på en random position varje game
 
+        randomspeed = Random.Range(5, 10); //randomiserar hastighet
+        halafart = false; //halspeed av
     }
 
     // Update is called once per frame
     void Update()
     {
-
         transform.Translate(2 * Time.deltaTime, 0, 0, Space.Self);
 
 
         if (Input.GetKeyUp(KeyCode.S))
         {
-            transform.Translate(1 * Time.deltaTime, 0, 0, Space.Self);
+            transform.Translate(randomspeed / -2 * Time.deltaTime, 0, 0, Space.Self); // tryck S så halveras speeden
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
+            transform.Rotate(0f, 0f, -rotationSpeed / 2 * Time.deltaTime); // tryck A för vänstersväng
 
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+            transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime); // tryck D för högersväng
 
 
         }
@@ -52,12 +57,21 @@ public class boll : MonoBehaviour
             newposition.x = -newposition.x;
         }
 
-        if (newposition.y < - 4.98f || newposition.y > 5.01f)
+        if (newposition.y < -4.98f || newposition.y > 5.01f)
         {
             newposition.y = -newposition.y;
 
         }
         transform.position = newposition;
+
+        Timer += Time.deltaTime;
+        if (Timer > variabel && Timer < variabel + 0.2)
+        {
+            Debug.Log(string.Format("timer"));
+
+            print(Timer);
+            variabel = (variabel + 1);
+        }
     }
 
 }
